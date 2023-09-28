7bcefbeb750f308a6b3382ed99ca2a4530a89146
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-09-28 17:35:59 -0500

    Test

--- displayCommands.py
+++ displayCommands.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def displayCommands():
     commands = open("/home/bsalas/cowrie.log", "r")
-    commands = commands.readlines()
-    return commands
+    displayText = ""
+    for line in commands.readlines():
+        if line.find("Command found") != -1:
+            displayText = displayText + line
+
+    return displayText
