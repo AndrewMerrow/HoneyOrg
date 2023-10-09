9e77473a59c9237dc19990f071e8d937cd49a85d
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-10-09 12:51:59 -0500

    display test

--- displayCommands.py
+++ displayCommands.py
@@ -5,6 +5,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def displayCommands():
+    displayText = getCommands()
+    #commands = open("/home/bsalas/cowrie.log", "r")
+    #displayText = []
+    #for line in commands.readlines():
+    #    if line.find("Command found") != -1:
+    #        displayText.append(line.rstrip("\n"))
+    #    elif line.find("login attempt") != -1:
+    #        displayText.append(line.rstrip("\n"))
+    #    elif line.find("Connection lost") != -1:
+    #        displayText.append(line.rstrip("\n"))
+    #    elif line.find("connection lost") != -1:
+    #        displayText.append(line.rstrip("\n"))
+
+
+
+    return displayText
+
+def getCommands():
     commands = open("/home/bsalas/cowrie.log", "r")
     displayText = []
     for line in commands.readlines():
@@ -16,7 +34,4 @@ def displayCommands():
             displayText.append(line.rstrip("\n"))
         elif line.find("connection lost") != -1:
             displayText.append(line.rstrip("\n"))
-
-
-
     return displayText
