33774387e6d780174b6001a42e062632c8aa47f9
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-10-10 13:00:49 -0500

    app test

--- app.py
+++ app.py
@@ -7,13 +7,17 @@ commands = open("/home/bsalas/cowrie.log", "r")
 displayText = []
 for line in commands.readlines():
     if line.find("Command found") != -1:
-        displayText.append(line.rstrip("\n"))
+        #displayText.append(line.rstrip("\n"))
+        displayText.append(line)
     elif line.find("login attempt") != -1:
-        displayText.append(line.rstrip("\n"))
+        #displayText.append(line.rstrip("\n"))
+        displayText.append(line)
     elif line.find("Connection lost") != -1:
-        displayText.append(line.rstrip("\n"))
+        #displayText.append(line.rstrip("\n"))
+        displayText.append(line)
     elif line.find("connection lost") != -1:
-        displayText.append(line.rstrip("\n"))
+        #displayText.append(line.rstrip("\n"))
+        displayText.append(line)
 #display_text = "test"
 
 app.layout = html.Div([
