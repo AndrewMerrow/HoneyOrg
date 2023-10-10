1fdceb58aa1a453878a2515169200be89d8896ca
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-10-10 13:04:48 -0500

    refresh change

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ for line in commands.readlines():
 #display_text = "test"
 
 app.layout = html.Div([
-    html.Meta(httpEquiv="refresh",content="60"),
+    html.Meta(httpEquiv="refresh",content="5"),
     html.Div(displayText, style={'whiteSpace': 'pre-line'})
 ])
 
