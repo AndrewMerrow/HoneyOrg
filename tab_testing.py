b4c6ba0cfe3862d48c3d993f4725d635cf91fd3d
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-05-07 09:44:56 -0500

    test

--- tab_testing.py
+++ tab_testing.py
@@ -118,19 +118,8 @@ def render_content(tab):
         return df.to_dict('records')
 
     elif tab == 'tab-test-2':
-        return html.Div([
-            html.H3('Tab content 2'),
-            dcc.Graph(
-                id='graph-2-tabs-dcc',
-                figure={
-                    'data': [{
-                        'x': [1, 2, 3],
-                        'y': [5, 10, 6],
-                        'type': 'bar'
-                    }]
-                }
-            )
-        ])
+        df = pd.DataFrame()
+        return df.to_dict('records')
 
 
 if __name__ == '__main__':
