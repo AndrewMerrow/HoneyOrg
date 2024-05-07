fc5c75d2a2390b9c29fb810b994e7bdcc429cb55
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-05-07 10:26:57 -0500

    test

--- tab_testing.py
+++ tab_testing.py
@@ -128,9 +128,9 @@ def render_content(tab, n_intervals):
             #print(line.rstrip('\n').split("[**]"))
             displayDict['type'].append('test')
             final_value = ""
-            #for value in line.rstrip('\n').split("[**]")[0:2]:
-            #    final_value += value
-            displayDict['value'].append('blah')
+            for value in line.rstrip('\n').split("[**]")[0:2]:
+                final_value += value
+            displayDict['value'].append(final_value)
             print(final_value)
             displayDict['ID'].append(0)
             break
